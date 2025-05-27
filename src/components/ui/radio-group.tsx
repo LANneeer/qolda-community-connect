6a9be4ff3de1a079cacc410a808f6625d8758d12
Temirlan Ilik <0penguin@mail.ru>
2025-05-27 16:00:49 +0500

    add-more-ui-tools

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { Circle } from "lucide-react"
@@ -41,4 +39,4 @@ const RadioGroupItem = React.forwardRef<
 })
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
 
-export { RadioGroup, RadioGroupItem } 
+export { RadioGroup, RadioGroupItem }
